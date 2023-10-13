cf981fd8c32c34488537aa88dc59f391432b9cf1
redamjibel <redamjibel@hotmail.com>
2023-10-13 14:58:11 -0700

    peerssayyoumustuseif

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,14 @@ import models
 class BaseModel:
 
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
+        if kwargs:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_t'] = datetime.strptime(kwargs['updated_at'],
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
