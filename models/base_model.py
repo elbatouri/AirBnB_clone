2202c679cbcd9b73027c4c66283ddfc35cc48fb1
redamjibel <redamjibel@hotmail.com>
2023-10-13 14:42:04 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -3,5 +3,14 @@
 The BaseModel class defines all common attributes/methods for other classes
 """
 
+import uuid
+from datetime import datetime
+import models
+
 class BaseModel:
+
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
