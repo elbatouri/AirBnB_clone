e74c4bbfc0f6b2719d2c3fecf9f8e3073bf38479
redamjibel <redamjibel@hotmail.com>
2023-10-13 14:59:31 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -20,4 +20,5 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
