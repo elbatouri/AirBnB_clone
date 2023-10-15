440a6ca662333f7f4d6e4fe0ebf3c06903fe86e2
redamjibel <redamjibel@hotmail.com>
2023-10-15 10:48:21 -0700

    testamenitytest

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -17,3 +17,16 @@ class TestAmenity(unittest.TestCase):
         """tests the length of amenity.__doc__"""
         doc = Amenity.__doc__
         self.assertGreater(len(doc), 1)
+
+    def test_doc_constructor(self):
+        """test Amenity doc constructor"""
+        doc = Amenity.__init__.__doc__
+        self.assertGreater(len(doc), 1)
+    def test_class(self):
+        """tests amnenity class attr & inheritance from basemodel"""
+        with self.subTest(msg='Inheritance'):
+            self.assertTrue(issubclass(Amenity, BaseModel))
+        with self.subTest(msg='Attributes'):
+            self.assertIsInstance(Amenity.name, str)
+if __name__ == '__main__':
+    unittest.main()
