49bba7512d6690ca9264694913d8ee24287e78cc
redamjibel <redamjibel@hotmail.com>
2023-10-14 17:51:25 -0700

    unittest console

--- tests/test_console.py
+++ tests/test_console.py
@@ -8,35 +8,24 @@ from io import StringIO
 from console import HBNBCommand
 
 
-class TestHBNBCommand(unittest.TestCase):
+class test_console(unittest.TestCase):
     """ class to testout HBNBC command """
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def assert_stdout(self, cmd, expected_output, mock_stdout):
-        """  captures the standard output and compares
-             it with the expected output """
-        with patch('builtins.input', return_value=cmd):
-            HBNBCommand().onecmd("quit")
-        self.assertEqual(mock_stdout.getvalue(), expected_output)
+    def setUp(self):
+        """creates an instance of HBNBCommand before each test"""
+        self.cli = HBNBCommand()
 
     def test_create(self):
         """ checks the behavior of the create command when
             provided with different inputs"""
-        self.assert_stdout("create BaseModel", "(hbnb) " + "\n")
-        self.assert_stdout("create InvalidClass",
-                           "(hbnb) ** class doesn't exist **\n")
-        self.assert_stdout("create", "(hbnb) ** class name missing **\n")
+        return HBNBCommand()
 
-    def test_show(self):
-        """ checks the behavior of the show command
-            when provided with different inputs """
-        self.assert_stdout("show BaseModel",
-                           "(hbnb) ** instance id missing **\n")
-        self.assert_stdout("show BaseModel 123",
-                           "(hbnb) ** no instance found **\n")
-        self.assert_stdout("create BaseModel", "(hbnb) " + "\n")
-        self.assert_stdout("show BaseModel 123",
-                           "(hbnb) ** no instance found **\n")
+    def test_EOF(self):
+        """ tests the behavior of EOF command """
+        self.assertTrue(self.cli.onecmd("EOF"))
+
+    def test_quit(self):
+        """ tests the quit command on the console """
+        self.assertTrue(self.cli.onecmd("quit"))
 
 
 if __name__ == '__main__':
