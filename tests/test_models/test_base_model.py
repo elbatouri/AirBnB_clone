e124ace1df8c6bb3fdd7bad7b2a24c5e5873b765
redamjibel <redamjibel@hotmail.com>
2023-10-16 16:48:32 -0700

    test basemodel attempt1

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,15 +1,47 @@
 #!/usr/bin/python3
+""" imorts modules to test base_model class"""
+import unittest
+import json
+import pep8
+from time import sleep
+
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
-                                   my_model_json[key]))
+
+class TestBaseModel(unittest.TestCase):
+    """The class in which the basemodel will be tested"""
+    def test_doc_module(self):
+        "Test length of doc"""
+        doc = BaseModel.__doc__
+        self.assertGreater(len(doc), 1)
+
+    def test_doc_constructor(self):
+        """tests the constructor '__init__'"""
+        doc = BaseModel.__init__.__doc__
+        self.assertGreater(len(doc), 1)
+
+    def test_first_task(self):
+        """Tests object creation and to_dict method"""
+        my_model = BaseModel()
+        self.assertIs(type(my_model), BaseModel)
+        my_model.name = "airbnb"
+        my_model.my_number = 2008
+        self.assertEqual(my_model.name, "airbnb")
+        self.assertEqual(my_model.my_number, 2008)
+        attr_types_json = {
+                "my_number": int,
+                "name": str,
+                "__class__": str,
+                "id": str,
+                "created_at": str,
+                "updated_at": str,
+        }
+        my_model_json = my_model.to_dict()
+        for key, value in attr_types_json.items():
+            with self.subTest(key=key, value=value):
+                self.assertIn(key, my_model_json)
+                self.assertIs(type(my_model_json[key]), value)
+
+
+if __name__ == '__main__':
+    unittest.main()
