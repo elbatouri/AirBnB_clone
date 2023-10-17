c4aa3d6adf6349b7d647b17a24e372f50dc8a52b
elbatouri rabi <elbatouri@gmail.com>
2023-10-16 18:00:16 -0700

    try 2 to fix console back

--- console.py
+++ console.py
@@ -4,7 +4,6 @@ import cmd
 from models import storage
 import shlex
 import re
-
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -18,7 +17,7 @@ class HBNBCommand(cmd.Cmd):
     """HBNBCommand class inheriting from cmd"""
     prompt = "(hbnb) "
     class_list = ["BaseModel", "User", "State", "City",
-                  "Amenity", "Place", "Review"]
+                  "Amenity", "Place", "Review", "destroy"]
     class_errors = {
         "missing_name": "** class name missing **",
         "not_exist": "** class doesn't exist **",
@@ -27,14 +26,18 @@ class HBNBCommand(cmd.Cmd):
         "missing_attribute": "** attribute name missing **",
         "missing_value": "** value missing **"
     }
+
     def do_EOF(self, line):
         """End-of-file marker"""
         return True
+
     def do_quit(self, line):
         """ Quit command to exit the program """
         return True
+
     def emptyline(self):
         pass
+
     def default(self, line):
         if line.endswith(".count()"):
             class_name = line.split(".")[0]
@@ -51,13 +54,22 @@ class HBNBCommand(cmd.Cmd):
             else:
                 print(self.class_errors["not_exist"])
 
-        elif line.endswith(".show()"):
-            class_name = line.split(".")[0]
+        elif re.match(r"(\w+)\.destroy\(\"([^\"]+)\"\)", line):
+            # Pattern matches <class name>.destroy("<instance_id>") format
+            match = re.match(r"(\w+)\.destroy\(\"([^\"]+)\"\)", line)
+            class_name, instance_id = match.groups()
+
             if class_name in self.class_list:
-                self.show_instances(class_name)
+                obj_dict = storage.all()
+                key = "{}.{}".format(class_name, instance_id)
+
+                if key in obj_dict:
+                    del obj_dict[key]
+                    storage.save()
+                else:
+                    print(self.class_errors["no_instance"])
             else:
                 print(self.class_errors["not_exist"])
-
         elif re.match(r"(\w+)\.update\(\"([^\"]+)\", "
                       r"\"([^\"]+)\", \"([^\"]+)\"\)", line):
             match = re.match(r"(\w+)\.update\(\"([^\"]"
@@ -74,8 +86,8 @@ class HBNBCommand(cmd.Cmd):
                     obj.save()
                 else:
                     print(self.class_errors["no_instance"])
-            else: print(self.class_errors["not_exist"])
-
+            else:
+                print(self.class_errors["not_exist"])
         elif re.match(r"(\w+)\.show\(\"([^\"]+)\"\)", line):
             # Pattern matches <class name>.destroy("<instance_id>") format
             match = re.match(r"(\w+)\.show\(\"([^\"]+)\"\)", line)
@@ -83,11 +95,12 @@ class HBNBCommand(cmd.Cmd):
             if class_name in self.class_list:
                 obj_dict = storage.all()
                 key = "{}.{}".format(class_name, instance_id)
+
                 if key in obj_dict:
+                    self.show_instances(class_name)
                 else:
                     print(self.class_errors["no_instance"])
-            else:
-                print(self.class_errors["not_exist"])
+
         else:
             print("Command not recognized")
 
@@ -98,11 +111,13 @@ class HBNBCommand(cmd.Cmd):
             if key.split('.')[0] == class_name:
                 count += 1
         return count
+
     def show_instances(self, class_name):
         obj_dict = storage.all()
         for key, value in obj_dict.items():
             if key.split('.')[0] == class_name:
                 print(value)
+
     def do_create(self, line):
         """Create a new instance of a class"""
         args = line.split()
@@ -114,6 +129,7 @@ class HBNBCommand(cmd.Cmd):
             new_instance = eval(args[0])()
             new_instance.save()
             print(new_instance.id)
+
     def do_show(self, line):
         """Show the details of an instance"""
         args = line.split()
@@ -149,20 +165,34 @@ class HBNBCommand(cmd.Cmd):
                 print(self.class_errors["no_instance"])
 
     def do_destroy(self, line):
-        """Destroy an instance by its ID"""
-        args = line.split()
-        if not args:
-            print(self.class_errors["missing_name"])
-        elif args[0] not in self.class_list:
+        """
+        Destroy an instance based on its ID.
+        Usage: <class name>.destroy(<id>)
+        Example: User.destroy(9aa2abd1-6ca1-4efe-9c6f-519bee7bc300)
+        """
+        args = shlex.split(line)
+        if len(args) != 2:
+            print("Invalid command format. Usage: <class name>.destroy(<id>)")
+            return
+
+        class_name = args[0]
+        instance_id = args[1]
+
+        # Check if the class exists
+        if class_name not in self.class_list:
             print(self.class_errors["not_exist"])
-        elif len(args) < 2:
-            print(self.class_errors["missing_id"])
+            return
+
+        # Construct the key to look for in the storage dictionary
+        key = "{}.{}".format(class_name, instance_id)
+        obj_dict = storage.all()
+
+        if key in obj_dict:
+            del obj_dict[key]
+            storage.save()
         else:
-            obj_dict = storage.all()
-            key = "{}.{}".format(args[0], args[1])
-            if key in obj_dict:
-                del obj_dict[key]
-                storage.save()
+            print(self.class_errors["no_instance"])
+
     def do_all(self, line):
         """Display all instances of a class or all classes"""
         obj_dict = storage.all()
@@ -177,27 +207,6 @@ class HBNBCommand(cmd.Cmd):
                 for key, value in obj_dict.items():
                     if value.__class__.__name__ == args[0]:
                         print(value)
-    def do_update(self, line):
-        """Update the attributes of an instance"""
-        args = line.split()
-        obj_dict = storage.all()
-        if not args:
-            print(self.class_errors["missing_name"])
-        elif args[0] not in self.class_list:
-            print(self.class_errors["not_exist"])
-        elif len(args) < 2:
-            print(self.class_errors["missing_id"])
-        else:
-            key = "{}.{}".format(args[0], args[1])
-            if key in obj_dict:
-                if len(args) < 3:
-                    print(self.class_errors["missing_attribute"])
-                elif len(args) < 4:
-                    print(self.class_errors["missing_value"])
-                else:
-                    obj = obj_dict[key]
-                    setattr(obj, args[2], args[3].replace("\"", ""))
-                    obj.save()
 
 
 if __name__ == '__main__':
